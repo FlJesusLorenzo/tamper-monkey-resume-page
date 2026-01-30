2d94d0ca22e99443db91872251fbc47d1a9dbc7f
FlJesusLorenzo <jesus.lorenzo@factorlibre.com>
2026-01-30 09:50:53 +0100

    Update ai_prompt resource URL in script.user.js

--- main/script.user.js
+++ main/script.user.js
@@ -7,7 +7,7 @@
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=factorlibre.com
 // @require      https://raw.githubusercontent.com/FlJesusLorenzo/tamper-monkey-imputar/refs/heads/main/main/scripts/utils.js
-// @resource     ai_prompt https://raw.githubusercontent.com/TU_USUARIO/TU_REPO/main/prompts/resumen.txt
+// @resource     ai_prompt https://github.com/FlJesusLorenzo/tamper-monkey-resume-page/raw/refs/heads/main/main/prompts/prompt.txt
 // @connect      generativelanguage.googleapis.com
 // @grant        GM_setValue
 // @grant        GM_getValue
